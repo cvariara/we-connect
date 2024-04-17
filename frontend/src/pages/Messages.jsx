0fe9b799ef3b7667a6c55edd9d5fb15c03a4b6d6
Christopher <cvariara@gmail.com>
2024-04-17 13:38:04 -0400

    Fixed loading for messages

--- frontend/src/pages/Messages.jsx
+++ frontend/src/pages/Messages.jsx
@@ -138,7 +138,7 @@ const Messages = ({ userData }) => {
     }
   }
 
-  if (loading || !messages.sender || receiverInfo || userData) {
+  if (loading || !messages.sender || !receiverInfo || !userData) {
     return  (
       <div className="loading">
         <RotatingLines

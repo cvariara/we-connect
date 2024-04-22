24100128bfeeceefa765c21013b818ab2931733c
Christopher <cvariara@gmail.com>
2024-04-22 13:23:07 -0400

    Removed extra socket folder

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,67 +1,3 @@
-// // import { Server } from "socket.io";
-// // import http from "http";
-// // import express from "express";
-
-// const { Server } = require('socket.io');
-// const http = require('http');
-// const express = require('express');
-
-// const app = express();
-// const server = http.createServer(app);
-// const io = new Server(server, {
-//   cors: {
-//     origin: ["http://localhost:5173"],
-//     methods: ["GET", "POST"],
-//   },
-// })
-
-
-
-// // const io = require('socket.io')(8900, {
-// //   cors: {
-// //     origin: "http://localhost:5173"
-// //   }
-// // });
-
-// let users = [];
-
-// const addUser = (userId, socketId) => {
-//   !users.some(user => user.userId === userId) &&
-//     users.push({ userId, socketId });
-// }
-
-// const removeUser = (socketId) => {
-//   users = users.filter((user) => user.socketId !== socketId);
-// }
-
-// const getUser = (userId) => {
-//   return users.find(user => user.userId === userId);
-// }
-
-// io.on('connection', (socket) => {
-//   // take user id and socket id from user
-//   socket.on("addUser", userId => {
-//     addUser(userId, socket.id);
-//     io.emit("getUsers", users);
-//   });
-
-//   // send & get message
-//   socket.on("sendMessage", ({senderId, receiverId, content}) => {
-//     const user = getUser(receiverId);
-//     io.to(user.socketId).emit("getMessage", {
-//       senderId,
-//       content,
-//     })
-//   })
-
-//   socket.on("disconnect", () => {
-//     removeUser(socket.id);
-//     io.emit("getUsers", users);
-//   });
-// });
-
-// module.exports = { app, server, io };
-
 const { Server } = require('socket.io');
 const http = require('http');
 const express = require('express');
@@ -70,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"], // Adjust origin as needed
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST"],
   },
 });
